080af029c8542859f7ce7abe8195ddbfa29b5429
Sergey Sukhorukov <sukhorukov1986@yandex.ru>
2022-11-29 10:50:39 +0000

    start on 3.2 task

--- .ccls-cache/@home@runner@Learning/main.c
+++ .ccls-cache/@home@runner@Learning/main.c
@@ -1,62 +1,19 @@
 #include <stdio.h>
-
-void squeeze(char s[], int c);
-void squeeze2(char s[], char ss[]);
-
-int string_length(const char *str);
+#include <string.h>
 
 int main()
 {
-  int length;
-  char str[] = "Big string";
-  char letter = 'g';
-  char str2[] = "pa";
+  char s[] = "a-f";
 
-  length = string_length(&str[0]);
+  char s2[60];
 
-  printf("%i\n", length);
-
-  squeeze2(str, str2);
-
-  printf("%s\n", str);
-
-  length = string_length(&str[0]);
-
-  printf("%i\n", length);
+  
 }
 
-void squeeze(char s[], int c)
-{
-  int i,  j;
-
-  for (i = j = 0; s[i] != '\0'; i++){
-    if (s[i] != c){
-      s[j++] = s[i];
-    }       
-  }
-        
-  s[j] = '\0';
-}
-
-void squeeze2(char s[], char ss[])
-{
-  int i, j, k;
-
-  for (i = 0; ss[i] != '\0'; i++){
-    for (k = j = 0; s[j] != '\0'; k++){
-      if(ss[i] != s[k]) {
-        s[j++] = s[k];
-      }
-    }        
-  }
-        
-  s[j] = '\0';
-}
-
-int string_length(const char *str) {
+void expand(char s1[], char s2[]) {
   int i;
-  for (i = 0; str[i]; i++) 
-    {}
-
-  return i;
+  
+  for (i = 0; i < strlen(s1);) {
+    
+  }
 }
