85e668dd0c553e997b4d3abc52d51c3c87c0829f
Sergey Sukhorukov <sukhorukov1986@yandex.ru>
2022-12-07 07:14:44 +0000

    Struct examples

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
-#include <string.h>
 
-int main() {
-  char s[] = "a-f";
-
-  char s2[60];
+void string_copy(char *dest, const char *src) {
+  int i;
+  for(i = 0; src[i]; i++)
+    dest[i] = src[i];
+  dest[i] = 0;
 }
 
-void expand(char s1[], char s2[]) {
-  int i;
+int main() {
+  enum { max_name_len = 64, max_group_len = 8 };
 
-  for (i = 0; i < strlen(s1); i++) {
-  }
+  struct student {
+    char name[max_name_len];
+    char sex;
+    int year_of_birth;
+    int major_code;
+    int year;
+    char group[max_group_len];
+    float average;
+  };
+
+  // Example 1
+  struct student st1;
+
+  string_copy(st1.name, "Otlichnikov Vasiliy Sergeevich");
+  st1.sex = 'm';
+  st1.year_of_birth = 1995;
+  st1.major_code = 231234;
+  st1.year = 3;
+  string_copy(st1.group, "231");
+  st1.average = 4.792;
+
+  // Example 2 during initialization
+  struct student st2 = {
+    "Otlichnikov Vasiliy Sergeevich",
+    'm', 1995, 231234, 3, "231", 4.792
+  };
+
+  struct student *p;
+  *p = st1;
+
+  // To get fields' value from pointer
+  int year = (*p).year;
+  float average = p->average;
 }
