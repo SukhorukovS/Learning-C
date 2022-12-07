078969808013fb8329411016682f132f8ba2cb22
Sergey Sukhorukov <sukhorukov1986@yandex.ru>
2022-12-07 06:49:12 +0000

    start with expand function

--- .ccls-cache/@home@runner@Learning/main.c
+++ .ccls-cache/@home@runner@Learning/main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+int main() {
   char s[] = "a-f";
 
   char s2[60];
-
-  
 }
 
 void expand(char s1[], char s2[]) {
   int i;
-  
-  for (i = 0; i < strlen(s1);) {
-    
+
+  for (i = 0; i < strlen(s1); i++) {
   }
 }
